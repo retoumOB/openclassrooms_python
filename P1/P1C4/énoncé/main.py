439841d7f7755b8b2b5fce6c111f059dece2025c
Vmp1991 <72992369+retoumOB@users.noreply.github.com>
2025-01-03 17:10:06 +0100

    Update main.py
    création et initialisation de variables, utilisation de f-string dans la fonction print pour afficher du texte comportant des variables

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,13 @@
 # Écrivez votre code ici !
+
+# Création d'une variable nom
+nom = "Vivien"
+# Création d'une variable age 
+age = 33
+
+# Affichage d'une phrase contenant votre nom et votre âge en utilisant la f-string
+print(f"Je m'appelle {nom} et j'ai {age} ans.") 
+
+# Modification la valeur de la variable age pour ajouter 10 ans à votre âge actuel.
+age = age + 10
+print(f"Je m'appelle {nom} et j'ai {age} ans maintenant.")
