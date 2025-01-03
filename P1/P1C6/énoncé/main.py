bca5127fc29ea5aaba0b2aa111184c6d8015e17f
Vmp1991 <72992369+retoumOB@users.noreply.github.com>
2025-01-03 18:42:39 +0100

    Update main.py
    utilisations de méthodes sur les listes

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,20 @@
 # Écrivez votre code ici !
+
+# liste 
+fruits = ["pomme", "banane", "orange"]
+print(fruits)
+# ajout d'un fruit à la liste
+fruits.append("kiwi")
+print(fruits)
+# Suppression d'un fruit à la liste
+fruits.remove("orange")
+print(fruits)
+# Modification du deuxième élément de la liste `fruits` en "ananas"
+fruits[1] = "ananas"
+print(fruits)
+# longueur de la liste fruits.
+print(len(fruits))
+print(fruits)
+# Trie de la liste fruits par ordre alphabétique
+fruits.sort()
+print(fruits)
