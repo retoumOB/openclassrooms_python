382d5622b74804111c261a30d6dd8c48dfeb262c
Vmp1991 <72992369+retoumOB@users.noreply.github.com>
2025-01-03 17:51:15 +0100

    Update main.py
    Affichage de valeurs de variables, et de leurs types

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,18 @@
 # Écrivez votre code ici !
+# Initialisation des variables
+nom = "Vivien"
+age = 33
+taille = 1.87
+est_etudiant = False
+
+# Affichage des valeurs et des noms de variables
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}"
+print(f"Est étudiant: {est_etudiant}"
+
+# Affichage du type de variables
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
